5000d60a445896a7e4a7217265bd04bcfec05186
Kris <mywtoday@gmail.com>
2026-06-29 17:54:17 +0300

    refactor(model): замена isEmpty() на isBlank(), добавление final к переменным конструктора

--- src/main/java/ru/aston/model/Student.java
+++ src/main/java/ru/aston/model/Student.java
@@ -5,11 +5,11 @@ public class Student {
 	private final double averageGrade;
 	private final String recordBookNumber;
 
-	public Student(String groupNumber, double averageGrade, String recordBookNumber) {
-		if (groupNumber == null || groupNumber.trim().isEmpty()) {
+	public Student(final String groupNumber, final double averageGrade, final String recordBookNumber) {
+		if (groupNumber == null || groupNumber.isBlank()) {
 			throw new IllegalArgumentException("Номер группы не может быть пустым или равным null.");
 		}
-		if (recordBookNumber == null || recordBookNumber.trim().isEmpty()) {
+		if (recordBookNumber == null || recordBookNumber.isBlank()) {
 			throw new IllegalArgumentException("Номер зачетной книжки не может быть пустым или равным null.");
 		}
 		if (averageGrade < 0.0 || averageGrade > 5.0) {
@@ -18,9 +18,9 @@ public class Student {
 			);
 		}
 
-		this.groupNumber = groupNumber.trim();
+		this.groupNumber = groupNumber;
 		this.averageGrade = averageGrade;
-		this.recordBookNumber = recordBookNumber.trim();
+		this.recordBookNumber = recordBookNumber;
 	}
 
 	public String getGroupNumber() {
