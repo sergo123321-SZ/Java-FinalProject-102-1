f5819b0cf85cbf93e3cdc512acf21fdf990fb5c7
Kris <mywtoday@gmail.com>
2026-06-29 18:22:44 +0300

    feature-builder

--- src/main/java/ru/aston/model/Car.java
+++ src/main/java/ru/aston/model/Car.java
@@ -1,30 +1,25 @@
 package ru.aston.model;
 
+import org.jetbrains.annotations.NotNull;
+
 import java.time.Year;
+import java.util.Objects;
 
 public class Car {
 	private final int power;
 	private final String model;
 	private final int productionYear;
 
-	public Car(final int power, final String model, final int productionYear) {
-		if (model == null || model.isBlank()) {
-			throw new IllegalArgumentException("Модель автомобиля не может быть пустой или равной null");
-		}
+	private Car(@NotNull final Builder builder) {
+		this(builder.power, builder.model, builder.productionYear);
+	}
 
-		if (power <= 0) {
-			throw new IllegalArgumentException("Мощность автомобиля должна быть больше 0. Передано: " + power);
-		}
+	public Car(final int power, @NotNull final String model, final int productionYear) {
 
-		int currentYear = Year.now().getValue();
-		if (productionYear < 1886 || productionYear > currentYear) {
-			throw new IllegalArgumentException(
-			        "Некорректный год производства. Должен быть от 1886 до текущего года." + currentYear
-			);
-		}
 
+
+		this.model = Objects.requireNonNull(model, "Модель автомобиля не может быть пустой или равной null");
 		this.power = power;
-		this.model = model;
 		this.productionYear = productionYear;
 	}
 
@@ -43,5 +38,33 @@ public class Car {
 	@Override
 	public String toString() {
 		return String.format("Автомобиль [Модель: %s, Мощность: %d л.с., Год: %d]", model, power, productionYear);
+	}
+
+	public static class Builder {
+		private int power;
+		private String model;
+		private int productionYear;
+
+		public Builder() {
+		}
+
+		public Builder power(int power) {
+			this.power = power;
+			return this;
+		}
+
+		public Builder model(String model) {
+			this.model = model;
+			return this;
+		}
+
+		public Builder productionYear(int productionYear) {
+			this.productionYear = productionYear;
+			return this;
+		}
+
+		public Car build() {
+			return new Car(this);
+		}
 	}
 }
--- src/main/java/ru/aston/model/Student.java
+++ src/main/java/ru/aston/model/Student.java
@@ -1,26 +1,27 @@
 package ru.aston.model;
 
+import java.util.Objects;
+
+import org.jetbrains.annotations.NotNull;
+
 public class Student {
 	private final String groupNumber;
 	private final double averageGrade;
 	private final String recordBookNumber;
 
-	public Student(final String groupNumber, final double averageGrade, final String recordBookNumber) {
-		if (groupNumber == null || groupNumber.isBlank()) {
-			throw new IllegalArgumentException("Номер группы не может быть пустым или равным null.");
-		}
-		if (recordBookNumber == null || recordBookNumber.isBlank()) {
-			throw new IllegalArgumentException("Номер зачетной книжки не может быть пустым или равным null.");
-		}
-		if (averageGrade < 0.0 || averageGrade > 5.0) {
-			throw new IllegalArgumentException(
-			        "Средний балл должен быть в диапазоне от 0,0 до 5,0. Дано:" + averageGrade
-			);
-		}
+	private Student(@NotNull final Builder builder) {
+		this(builder.groupNumber, builder.averageGrade, builder.recordBookNumber);
+	}
 
-		this.groupNumber = groupNumber;
+	public Student(
+	        @NotNull final String groupNumber, final double averageGrade, @NotNull final String recordBookNumber
+	) {
+
+
+
+		this.groupNumber = Objects.requireNonNull(groupNumber, "Номер группы должен быть указан");
+		this.recordBookNumber = Objects.requireNonNull(recordBookNumber, "Номер зачетной книжки должен быть указан");
 		this.averageGrade = averageGrade;
-		this.recordBookNumber = recordBookNumber;
 	}
 
 	public String getGroupNumber() {
@@ -41,5 +42,33 @@ public class Student {
 		        "Студент [Группа: %s, Средний балл: %.2f, Номер зачетной книжки: %s]", groupNumber, averageGrade,
 		        recordBookNumber
 		);
+	}
+
+	public static class Builder {
+		private String groupNumber;
+		private double averageGrade = 0.0;
+		private String recordBookNumber;
+
+		public Builder() {
+		}
+
+		public Builder groupNumber(String groupNumber) {
+			this.groupNumber = groupNumber;
+			return this;
+		}
+
+		public Builder averageGrade(double averageGrade) {
+			this.averageGrade = averageGrade;
+			return this;
+		}
+
+		public Builder recordBookNumber(String recordBookNumber) {
+			this.recordBookNumber = recordBookNumber;
+			return this;
+		}
+
+		public Student build() {
+			return new Student(this);
+		}
 	}
 }
