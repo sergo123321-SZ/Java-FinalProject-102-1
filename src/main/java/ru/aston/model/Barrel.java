7a498274f45a01b400aa461808edb0a18a2c8b15
Kris <mywtoday@gmail.com>
2026-06-29 18:22:44 +0300

    feature-builder

--- src/main/java/ru/aston/model/Barrel.java
+++ src/main/java/ru/aston/model/Barrel.java
@@ -1,25 +1,25 @@
 package ru.aston.model;
 
+import java.util.Objects;
+
+import org.jetbrains.annotations.NotNull;
+
 public class Barrel {
 	private final double volume;
 	private final String storedMaterial;
 	private final String barrelMaterial;
 
-	public Barrel(final double volume, final String storedMaterial, final String barrelMaterial) {
-		if (storedMaterial == null || storedMaterial.isBlank()) {
-			throw new IllegalArgumentException("Хранимый материал не может быть пустым или равным null");
-		}
-		if (barrelMaterial == null || barrelMaterial.isBlank()) {
-			throw new IllegalArgumentException("Материал бочки не может быть пустым или равным null");
-		}
+	private Barrel(@NotNull final Builder builder) {
+		this(builder.volume, builder.storedMaterial, builder.barrelMaterial);
+	}
 
-		if (volume <= 0.0) {
-			throw new IllegalArgumentException("Объем бочки должен быть больше 0. Передано: " + volume);
-		}
+	public Barrel(final double volume, @NotNull final String storedMaterial, @NotNull final String barrelMaterial) {
 
 		this.volume = volume;
-		this.storedMaterial = storedMaterial;
-		this.barrelMaterial = barrelMaterial;
+		this.storedMaterial = Objects.requireNonNull(storedMaterial, "Хранимый материал должен быть указан");
+		this.barrelMaterial = Objects.requireNonNull(barrelMaterial, "Материал бочки должен быть указан").trim();
+
+
 	}
 
 	public double getVolume() {
@@ -39,5 +39,33 @@ public class Barrel {
 		return String.format(
 		        "Бочка [Объем: %.1f л, Содержимое: %s, Материал бочки: %s]", volume, storedMaterial, barrelMaterial
 		);
+	}
+
+	public static class Builder {
+		private double volume;
+		private String storedMaterial;
+		private String barrelMaterial;
+
+		public Builder() {
+		}
+
+		public Builder volume(double volume) {
+			this.volume = volume;
+			return this;
+		}
+
+		public Builder storedMaterial(String storedMaterial) {
+			this.storedMaterial = storedMaterial;
+			return this;
+		}
+
+		public Builder barrelMaterial(String barrelMaterial) {
+			this.barrelMaterial = barrelMaterial;
+			return this;
+		}
+
+		public Barrel build() {
+			return new Barrel(this);
+		}
 	}
 }
