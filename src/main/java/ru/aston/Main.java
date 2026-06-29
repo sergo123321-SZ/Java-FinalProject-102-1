9672e3c04ad24d7a0c372fd56efc50bec9fa65b3
Kris <mywtoday@gmail.com>
2026-06-29 10:29:40 +0300

    format fixed

--- src/main/java/ru/aston/Main.java
+++ src/main/java/ru/aston/Main.java
@@ -1,12 +1,12 @@
 package ru.aston;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
+	public static void main(String[] args) {
+		System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("i = " + i);
-        }
+		for (int i = 1; i <= 5; i++) {
+			System.out.println("i = " + i);
+		}
 
-    }
+	}
 }
