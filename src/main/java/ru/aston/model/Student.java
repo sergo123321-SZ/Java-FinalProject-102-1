cbc5dfc72cf7b91a70f7206ca64bca80552bc2bc
Kris <mywtoday@gmail.com>
2026-06-29 18:15:19 +0300

    refactor(model): исправлено на "номер зачетной книжки"

--- src/main/java/ru/aston/model/Student.java
+++ src/main/java/ru/aston/model/Student.java
@@ -38,7 +38,7 @@ public class Student {
 	@Override
 	public String toString() {
 		return String.format(
-		        "Студент [Группа: %s, Средний балл: %.2f, Номер записной книжки: %s]", groupNumber, averageGrade,
+		        "Студент [Группа: %s, Средний балл: %.2f, Номер зачетной книжки: %s]", groupNumber, averageGrade,
 		        recordBookNumber
 		);
 	}
