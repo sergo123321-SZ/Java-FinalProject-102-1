2dc72153eb1e15766f46ec844000f8f55b792247
Kris <mywtoday@gmail.com>
2026-06-30 15:56:16 +0300

    feature-comparators

--- src/main/java/ru/aston/model/Barrel.java
+++ src/main/java/ru/aston/model/Barrel.java
@@ -2,10 +2,10 @@ package ru.aston.model;
 
 import org.jetbrains.annotations.NotNull;
 
+import java.util.Comparator;
 import java.util.Objects;
 
-
-public class Barrel {
+public class Barrel implements Comparable<Barrel> {
 	private final double volume;
 	private final String storedMaterial;
 	private final String barrelMaterial;
@@ -15,7 +15,6 @@ public class Barrel {
 	}
 
 	public Barrel(final double volume, @NotNull final String storedMaterial, @NotNull final String barrelMaterial) {
-
 		this.volume = volume;
 		this.storedMaterial = Objects.requireNonNull(storedMaterial, "Хранимый материал должен быть указан");
 		this.barrelMaterial = Objects.requireNonNull(barrelMaterial, "Материал бочки должен быть указан");
@@ -39,6 +38,35 @@ public class Barrel {
 				"Бочка [Объем: %.1f л, Содержимое: %s, Материал бочки: %s]", volume, storedMaterial, barrelMaterial
 		);
 	}
+
+	@Override
+	public int compareTo(@NotNull Barrel other) {
+		return Double.compare(this.volume, other.volume);
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o)
+			return true;
+		if (o == null || getClass() != o.getClass())
+			return false;
+		Barrel barrel = (Barrel) o;
+		return Double.compare(barrel.volume, volume) == 0 && storedMaterial.equals(barrel.storedMaterial)
+				&& barrelMaterial.equals(barrel.barrelMaterial);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(volume, storedMaterial, barrelMaterial);
+	}
+
+	public static final Comparator<Barrel> BY_STORED_MATERIAL = Comparator.comparing(Barrel::getStoredMaterial);
+
+	public static final Comparator<Barrel> BY_BARREL_MATERIAL = Comparator.comparing(Barrel::getBarrelMaterial);
+
+	public static final Comparator<Barrel> BY_MATERIAL_THEN_VOLUME_DESC = Comparator
+			.comparing(Barrel::getStoredMaterial)
+			.thenComparing(Comparator.comparingDouble(Barrel::getVolume).reversed());
 
 	public static class Builder {
 		private double volume;
--- src/main/java/ru/aston/model/Car.java
+++ src/main/java/ru/aston/model/Car.java
@@ -2,10 +2,10 @@ package ru.aston.model;
 
 import org.jetbrains.annotations.NotNull;
 
-import java.time.Year;
+import java.util.Comparator;
 import java.util.Objects;
 
-public class Car {
+public class Car implements Comparable<Car> {
 	private final int power;
 	private final String model;
 	private final int productionYear;
@@ -15,8 +15,6 @@ public class Car {
 	}
 
 	public Car(final int power, @NotNull final String model, final int productionYear) {
-
-
 		this.model = Objects.requireNonNull(model, "Модель автомобиля не может быть пустой или равной null");
 		this.power = power;
 		this.productionYear = productionYear;
@@ -38,6 +36,33 @@ public class Car {
 	public String toString() {
 		return String.format("Автомобиль [Модель: %s, Мощность: %d л.с., Год: %d]", model, power, productionYear);
 	}
+
+	@Override
+	public int compareTo(@NotNull Car other) {
+		return this.model.compareTo(other.model);
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o)
+			return true;
+		if (o == null || getClass() != o.getClass())
+			return false;
+		Car car = (Car) o;
+		return power == car.power && productionYear == car.productionYear && model.equals(car.model);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(power, model, productionYear);
+	}
+
+	public static final Comparator<Car> BY_POWER = Comparator.comparingInt(Car::getPower);
+
+	public static final Comparator<Car> BY_YEAR = Comparator.comparingInt(Car::getProductionYear);
+
+	public static final Comparator<Car> BY_MODEL_THEN_POWER_DESC = Comparator.comparing(Car::getModel)
+			.thenComparing(Comparator.comparingInt(Car::getPower).reversed());
 
 	public static class Builder {
 		private int power;
