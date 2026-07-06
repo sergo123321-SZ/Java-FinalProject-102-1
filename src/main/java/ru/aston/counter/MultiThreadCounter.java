ba94bae1dd1b0009d95abdb44b89c3e75fb1b708
Roman <roman.arychev@gmail.com>
2026-07-06 15:57:46 +0300

    task - multithreading counter
    # Conflicts:
    #	pom.xml

---
+++ src/main/java/ru/aston/counter/MultiThreadCounter.java
@@ -0,0 +1,92 @@
+package ru.aston.counter;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Objects;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.Future;
+
+public class MultiThreadCounter {
+
+	public static Long countParallelArray(Object[] objects, @NotNull Object object, int threadsCount)
+			throws ExecutionException, InterruptedException
+	{
+		if (threadsCount <= 0) {
+			throw new IllegalArgumentException("Threads count must be greater than 0");
+		}
+
+		// Разделяем коллекцию на части и запускаем потоки
+		List<Future<Integer>> futures = new ArrayList<>();
+		int chunkSize = objects.length / threadsCount;
+
+		for (int i = 0; i < threadsCount; i++) {
+			int start = i * chunkSize;
+			int end = (i == threadsCount - 1) ? objects.length : start + chunkSize;
+
+			Future<Integer> future = CompletableFuture.supplyAsync(() -> {
+				int count = 0;
+				for (int j = start; j < end; j++) {
+					if (Objects.equals(objects[j], object)) {
+						count++;
+					}
+				}
+				return count;
+			});
+			futures.add(future);
+		}
+
+		// Собираем результаты
+		Long totalCount = 0L;
+		for (Future<Integer> future : futures) {
+			totalCount += future.get();
+		}
+
+		return totalCount;
+	}
+
+	public static Long countParallelSubList(Collection<?> objects, @NotNull Object object, int threadsCount) {
+		if (threadsCount <= 0) {
+			throw new IllegalArgumentException("Threads count must be greater than 0");
+		}
+
+		Long totalCount = 0L;
+
+		try (ForkJoinPool customThreadPool = new ForkJoinPool(threadsCount)) {
+			int chunkSize = objects.size() / threadsCount;
+
+			List<?> searchList = objects.stream().toList();
+			List<Future<Long>> futures = new ArrayList<>();
+
+			for (int i = 0; i < threadsCount; i++) {
+				int start = i * chunkSize;
+				int end = (i == threadsCount - 1) ? objects.size() : start + chunkSize;
+
+				Future<Long> future = customThreadPool.submit(() -> searchList.subList(start, end)
+						.parallelStream() // Делаем стрим параллельным
+						.filter(o -> Objects.equals(o, object))
+						.count());
+				futures.add(future);
+			}
+
+			// Собираем результаты
+			for (Future<Long> future : futures) {
+				totalCount += future.get();
+			}
+		}
+		catch (ExecutionException | InterruptedException e) {
+			throw new RuntimeException(e);
+		}
+		return totalCount;
+	}
+
+	public static Long countParallelStream(Collection<?> objects, @NotNull Object object) {
+		return objects.parallelStream()
+				.filter(o -> Objects.equals(o, object))
+				.count();
+	}
+}
---
+++ src/test/java/ru/aston/counter/StudentCounterTest.java
@@ -0,0 +1,102 @@
+package ru.aston.counter;
+
+import org.apache.commons.lang3.RandomStringUtils;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import ru.aston.model.Student;
+
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.ExecutionException;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class StudentCounterTest {
+	static List<Student> students;
+	static Student targetStudent;
+	static final String TARGET_GROUP_NUMBER = "АЯ-51";
+	static final String TARGET_RECORD_BOOK_NUMBER = "104857";
+	static final Double TARGET_AVERAGE_GRADE = 5.0;
+
+	@BeforeAll
+	static void setUp() {
+		Random random = new Random();
+		students = Stream.generate(() -> Student.builder()
+				.setGroupNumber(RandomStringUtils.insecure().nextNumeric(10))
+				.setAverageGrade(random.nextDouble())
+				.setRecordBookNumber(RandomStringUtils.insecure().nextAlphabetic(10))
+				.build())
+				.limit(100_000)
+				.collect(Collectors.toList());
+
+		// добавим несколько целевых
+		students.addAll(List.of(Student.builder()
+				.setGroupNumber(TARGET_GROUP_NUMBER)
+				.setAverageGrade(TARGET_AVERAGE_GRADE)
+				.setRecordBookNumber(TARGET_RECORD_BOOK_NUMBER)
+				.build(), Student.builder()
+						.setGroupNumber(TARGET_GROUP_NUMBER)
+						.setAverageGrade(TARGET_AVERAGE_GRADE)
+						.setRecordBookNumber(TARGET_RECORD_BOOK_NUMBER)
+						.build(), Student.builder()
+								.setGroupNumber(TARGET_GROUP_NUMBER)
+								.setAverageGrade(TARGET_AVERAGE_GRADE)
+								.setRecordBookNumber(TARGET_RECORD_BOOK_NUMBER)
+								.build()));
+
+		targetStudent = Student.builder()
+				.setGroupNumber(TARGET_GROUP_NUMBER)
+				.setAverageGrade(TARGET_AVERAGE_GRADE)
+				.setRecordBookNumber(TARGET_RECORD_BOOK_NUMBER)
+				.build();
+	}
+
+	@Test
+	void countParallelArrayTest() throws ExecutionException, InterruptedException {
+		long start1 = System.nanoTime();
+		Long result = MultiThreadCounter.countParallelArray(students.toArray(), targetStudent, 1);
+		long end1 = System.nanoTime();
+		long oneThreadTime = (end1 - start1) / 1_000_000;
+		System.out.println("Thread One: " + oneThreadTime + " ms");
+
+		assertEquals(3, result);
+
+		long start2 = System.nanoTime();
+		result = MultiThreadCounter.countParallelArray(students.toArray(), targetStudent, 4);
+		long end2 = System.nanoTime();
+		long fourThreadsTime = (end2 - start2) / 1_000_000;
+		System.out.println("Four threads: " + fourThreadsTime + " ms");
+
+		assertEquals(3, result);
+	}
+
+	@Test
+	void parallelSubListTest() {
+		long start1 = System.nanoTime();
+		Long result = MultiThreadCounter.countParallelSubList(students, targetStudent, 1);
+		long end1 = System.nanoTime();
+		long oneThreadTime = (end1 - start1) / 1_000_000;
+		System.out.println("Thread One: " + oneThreadTime + " ms");
+
+		assertEquals(3, result);
+
+		long start2 = System.nanoTime();
+		result = MultiThreadCounter.countParallelSubList(students, targetStudent, 2);
+		long end2 = System.nanoTime();
+		long twoThreadsTime = (end2 - start2) / 1_000_000;
+		System.out.println("Two threads: " + twoThreadsTime + " ms");
+
+		assertEquals(3, result);
+	}
+
+	@Test
+	void parallelStreamTest() {
+		long start1 = System.nanoTime();
+		assertEquals(3, MultiThreadCounter.countParallelStream(students, targetStudent));
+		long end1 = System.nanoTime();
+		long parallelStreamTime = (end1 - start1) / 1_000_000;
+		System.out.println("parallelStreamTest: " + parallelStreamTime + " ms");
+	}
+}
