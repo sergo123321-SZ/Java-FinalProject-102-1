7e25d44be30bb498d2c6b6314bfa7680a637f18c
amchik <am4uk.nmcs@gmail.com>
2026-07-08 16:38:22 +0400

    Random Generator

--- src/main/java/ru/aston/randomgenerator/CarRandomGenerator.java
+++ src/main/java/ru/aston/randomgenerator/CarRandomGenerator.java
@@ -3,24 +3,20 @@ package ru.aston.randomgenerator;
 import net.datafaker.Faker;
 import ru.aston.model.Car;
 
-import java.util.ArrayList;
-import java.util.Collection;
+import java.util.List;
 import java.util.Locale;
 import java.time.Year;
 
 public class CarRandomGenerator {
 	private static final Faker faker = new Faker(Locale.forLanguageTag("ru"));
 
-	public static Collection<Car> generate(int size) {
-		Collection<Car> result = new ArrayList<>(size);
-		for (int i = 0; i < size; ++i) {
-			Car item = Car.builder()
-					.setModel(faker.vehicle().model())
-					.setPower(faker.number().numberBetween(60, 600))
-					.setProductionYear(faker.number().numberBetween(1950, Year.now().getValue()))
-					.build();
-			result.add(item);
-		}
-		return result;
+	public static List<Car> generate(int size) {
+		return faker.collection(
+				() -> Car.builder()
+						.setModel(faker.vehicle().model())
+						.setPower(faker.number().numberBetween(60, 600))
+						.setProductionYear(faker.number().numberBetween(1950, Year.now().getValue()))
+						.build()
+		).len(size).generate();
 	}
 }
--- src/main/java/ru/aston/randomgenerator/StudentRandomGenerator.java
+++ src/main/java/ru/aston/randomgenerator/StudentRandomGenerator.java
@@ -3,23 +3,19 @@ package ru.aston.randomgenerator;
 import net.datafaker.Faker;
 import ru.aston.model.Student;
 
-import java.util.ArrayList;
-import java.util.Collection;
+import java.util.List;
 import java.util.Locale;
 
 public class StudentRandomGenerator {
 	private static final Faker faker = new Faker(Locale.forLanguageTag("ru"));
 
-	public static Collection<Student> generate(int size) {
-		Collection<Student> result = new ArrayList<>(size);
-		for (int i = 0; i < size; ++i) {
-			Student item = Student.builder()
-					.setGroupNumber(faker.regexify("[А-Я]{2}-\\d{2}"))
-					.setAverageGrade(Math.round(faker.number().randomDouble(2, 2, 5) * 100.0) / 100.0)
-					.setRecordBookNumber(faker.regexify("\\d{6}"))
-					.build();
-			result.add(item);
-		}
-		return result;
+	public static List<Student> generate(int size) {
+		return faker.collection(
+				() -> Student.builder()
+						.setGroupNumber(faker.regexify("[А-Я]{2}-\\d{2}"))
+						.setAverageGrade(Math.round(faker.number().randomDouble(2, 2, 5) * 100.0) / 100.0)
+						.setRecordBookNumber(faker.regexify("\\d{6}"))
+						.build()
+		).len(size).generate();
 	}
 }
