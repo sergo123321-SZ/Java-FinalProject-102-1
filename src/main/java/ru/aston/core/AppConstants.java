be895735179ee9ee013834e7517b2be98b42facd
Sergey Zastrovnyh <sergeizastrovnyh@yandex.com>
2026-07-08 20:59:33 +0600

    Add AppConstants class and app.properties for configuration management

---
+++ src/main/java/ru/aston/core/AppConstants.java
@@ -0,0 +1,38 @@
+package ru.aston.core;
+
+
+public class AppConstants {
+	private AppConstants() {
+	}
+
+	public enum SortType {
+		ASC("ASC"),
+		DESC("DESC"),
+		SPECIAL("SPECIAL");
+
+		private final String value;
+
+		SortType(String value) {
+			this.value = value;
+		}
+
+		public String getValue() {
+			return value;
+		}
+	}
+
+	public enum WriteType {
+		APPEND("A"),
+		OVERWRITE("W");
+
+		private final String value;
+
+		WriteType(String value) {
+			this.value = value;
+		}
+
+		public String getValue() {
+			return value;
+		}
+	}
+}
