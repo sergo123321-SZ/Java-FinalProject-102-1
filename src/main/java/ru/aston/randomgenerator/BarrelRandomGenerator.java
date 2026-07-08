8b6f92d825093d94da57f21eacd50e1c071f2a02
amchik <am4uk.nmcs@gmail.com>
2026-07-08 16:38:22 +0400

    Random Generator

--- src/main/java/ru/aston/randomgenerator/BarrelRandomGenerator.java
+++ src/main/java/ru/aston/randomgenerator/BarrelRandomGenerator.java
@@ -3,25 +3,21 @@ package ru.aston.randomgenerator;
 import net.datafaker.Faker;
 import ru.aston.model.Barrel;
 
-import java.util.ArrayList;
-import java.util.Collection;
+import java.util.List;
 import java.util.Locale;
 
 public class BarrelRandomGenerator {
 	private static final Faker faker = new Faker(Locale.forLanguageTag("ru"));
 
-	public static Collection<Barrel> generate(int size) {
-		Collection<Barrel> result = new ArrayList<>(size);
-		for (int i = 0; i < size; ++i) {
-			Barrel item = Barrel.builder()
-					.setVolume(Math.round(faker.number().randomDouble(1, 10, 500) * 10.0) / 10.0)
-					.setStoredMaterial(
-							faker.options().option("Нефть", "Бензин", "Мазут", "Вода", "Вино", "Пиво", "Мед", "Сок")
-					)
-					.setBarrelMaterial(faker.options().option("Дуб", "Сосна", "Сталь", "Пластик", "Алюминий"))
-					.build();
-			result.add(item);
-		}
-		return result;
+	public static List<Barrel> generate(int size) {
+		return faker.collection(
+				() -> Barrel.builder()
+						.setVolume(Math.round(faker.number().randomDouble(1, 10, 500) * 10.0) / 10.0)
+						.setStoredMaterial(
+								faker.options().option("Нефть", "Бензин", "Мазут", "Вода", "Вино", "Пиво", "Мед", "Сок")
+						)
+						.setBarrelMaterial(faker.options().option("Дуб", "Сосна", "Сталь", "Пластик", "Алюминий"))
+						.build()
+		).len(size).generate();
 	}
 }
