248084f72cdbac01c1c6f96cb651c949779b76f3
Sergey Zastrovnyh <sergeizastrovnyh@yandex.com>
2026-07-10 16:45:08 +0600

    Implement ModelGeneration class for model creation V1

---
+++ src/main/java/ru/aston/command/ModelGeneration.java
@@ -0,0 +1,72 @@
+package ru.aston.command;
+
+
+import org.apache.commons.cli.CommandLine;
+import org.jetbrains.annotations.NotNull;
+import ru.aston.core.AppConstants;
+import ru.aston.core.AppConstants.WriteType;
+import ru.aston.randomgenerator.BarrelRandomGenerator;
+import ru.aston.randomgenerator.CarRandomGenerator;
+import ru.aston.randomgenerator.StudentRandomGenerator;
+
+import java.util.List;
+
+public class ModelGeneration extends BaseExecutor {
+	private int size = 0;
+	private AppConstants.WriteType generationRule;
+
+	ModelGeneration() {
+		super(List.of(CommandProcessor.CommandStep.CREATE));
+	}
+
+	@Override
+	void doExec(@NotNull CommandLine options, @NotNull ExecutionData executionData) {
+		if (executionData.modelType == null) {
+			throw new IllegalArgumentException("'modelType' is null. MUST be assigned before execution!");
+		}
+
+		switch (executionData.modelType) {
+			case CARS:
+				executionData.carCollection = CarRandomGenerator.generate(size);
+				break;
+			case STUDENTS:
+				executionData.studentCollection = StudentRandomGenerator.generate(size);
+				break;
+			case BARRELS:
+				executionData.barrelCollection = BarrelRandomGenerator.generate(size);
+				break;
+			default:
+				throw new IllegalArgumentException("'modelType' is unknown. MUST be assigned before execution!");
+		}
+	}
+
+	@Override
+	public boolean checkOptions(@NotNull CommandLine commandLine) {
+		if (!super.checkOptions(commandLine)) {
+			return false;
+		}
+
+		try {
+			String createOptionValue = commandLine.getOptionValue(CommandProcessor.CommandStep.CREATE.shortOpt);
+			size = Integer.parseInt(createOptionValue);
+		}
+		catch (Exception e) {
+			lastError = "'create' option must be a positive integer";
+			return false;
+		}
+
+		try {
+			// String generationRuleValue =
+			// commandLine.getOptionValue(CommandProcessor.CommandStep.GENERATION_RULE.shortOpt);
+			// generationRule = AppConstants.WriteType.valueOf(generationRuleValue);
+			generationRule = WriteType.OVERWRITE;
+		}
+		catch (Exception e) {
+			lastError = "'generationRule' option is invalid";
+			return false;
+		}
+
+
+		return true;
+	}
+}
--- src/main/java/ru/aston/command/ModelSelectionExecutor.java
+++ src/main/java/ru/aston/command/ModelSelectionExecutor.java
@@ -4,27 +4,11 @@ package ru.aston.command;
 import org.apache.commons.cli.CommandLine;
 import org.jetbrains.annotations.NotNull;
 import ru.aston.core.AppConstants;
-import ru.aston.model.Barrel;
-import ru.aston.model.Car;
-import ru.aston.model.Student;
-
-import java.util.Collection;
 import java.util.List;
 
 public class ModelSelectionExecutor extends BaseExecutor {
-	private Collection<Car> carCollection = null;
-	private Collection<Student> studentCollection = null;
-	private Collection<Barrel> barrelCollection = null;
-
-	public ModelSelectionExecutor(
-			Collection<Car> carCollection,
-			Collection<Student> studentCollection,
-			Collection<Barrel> barrelCollection)
-	{
+	public ModelSelectionExecutor() {
 		super(List.of(CommandProcessor.CommandStep.MODEL));
-		this.carCollection = carCollection;
-		this.studentCollection = studentCollection;
-		this.barrelCollection = barrelCollection;
 	}
 
 	@Override
