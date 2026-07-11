afc1464d2288e2d26af62f50bce2a875bc5cc2a1
Sergey Zastrovnyh <sergeizastrovnyh@yandex.com>
2026-07-12 01:57:23 +0600

    Refactor command execution flow by introducing InteractiveConsoleApplication; update command processing and improve message properties

--- src/main/java/ru/aston/Main.java
+++ src/main/java/ru/aston/Main.java
@@ -1,18 +1,12 @@
 package ru.aston;
 
-import ru.aston.command.CommandProcessor;
+
+import ru.aston.core.InteractiveConsoleApplication;
+
 
 public class Main {
 	public static void main(String[] args) {
 
-		CommandProcessor p = new CommandProcessor();
-
-		p.executeCommands(new String[]{
-				"-M",
-				"BARRELS",
-				"-C",
-				"10",
-				"-D"
-		});
+		new InteractiveConsoleApplication().exec();
 	}
 }
