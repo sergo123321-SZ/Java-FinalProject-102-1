0c1352ade1abeb3981b908630ad1059ddf60417c
Sergey Zastrovnyh <sergeizastrovnyh@yandex.com>
2026-07-12 01:28:33 +0600

    Refactor command descriptions to remove conflicting options and simplify method signatures

--- src/main/java/ru/aston/command/CommandProcessor.java
+++ src/main/java/ru/aston/command/CommandProcessor.java
@@ -51,39 +51,25 @@ public class CommandProcessor {
 			case HELP -> TranslationManager.getHelpText("1.0", "TeamName");
 			case MODEL -> TranslationManager.getModelOptionDescription(getAcceptableOptionsString(step));
 			case LENGTH -> TranslationManager.getLengthOptionDescription(
-					getRequiredStepsString(step),
-					getConflictingStepsString(step)
+					getRequiredStepsString(step)
 			);
 			case DISPLAY -> TranslationManager.getDisplayOptionDescription(
-					getRequiredStepsString(step),
-					getConflictingStepsString(step)
+					getRequiredStepsString(step)
 			);
 			case SORT -> TranslationManager.getSortOptionDescription(
 					AppConstants.SortType.ASC.getValue(),
 					AppConstants.SortType.DESC.getValue(),
 					AppConstants.SortType.SPECIAL.getValue(),
-					getRequiredStepsString(step),
-					getConflictingStepsString(step)
+					getRequiredStepsString(step)
 			);
 			case CREATE -> TranslationManager.getCreateOptionDescription(
-					getRequiredStepsString(step),
-					getConflictingStepsString(step)
-			);
-			case FILE -> TranslationManager.getFileOptionDescription(
-					getRequiredStepsString(step),
-					getConflictingStepsString(step)
+					getRequiredStepsString(step)
 			);
 			case EXPORT -> TranslationManager.getExportOptionDescription(
-					AppConstants.WriteType.APPEND.getValue(),
-					AppConstants.WriteType.OVERWRITE.getValue(),
-					getRequiredStepsString(step),
-					getConflictingStepsString(step)
+					getRequiredStepsString(step)
 			);
 			case IMPORT -> TranslationManager.getImportOptionDescription(
-					AppConstants.WriteType.APPEND.getValue(),
-					AppConstants.WriteType.OVERWRITE.getValue(),
-					getRequiredStepsString(step),
-					getConflictingStepsString(step)
+					getRequiredStepsString(step)
 			);
 			default -> throw new IllegalArgumentException("Unknown command step: " + step);
 		};
@@ -106,14 +92,6 @@ public class CommandProcessor {
 		return String.join(" | ", step.requiredSteps.stream().map(s -> s.longOpt).toList());
 	}
 
-	private @NotNull String getConflictingStepsString(@NotNull CommandStep step) {
-		if (step.conflictingSteps.isEmpty()) {
-			return "-";
-		}
-
-		return String.join(" | ", step.conflictingSteps.stream().map(s -> s.longOpt).toList());
-	}
-
 	enum CommandStep {
 		EXIT("Q", "exit", false),
 		HELP("H", "help", false),
@@ -122,18 +100,12 @@ public class CommandProcessor {
 		DISPLAY("D", "display", false, List.of(MODEL)),
 		SORT("S", "sort", true, List.of(MODEL)),
 		CREATE("C", "create", true, List.of(MODEL)),
-		FILE("F", "file", true, List.of(MODEL)),
-		EXPORT("E", "export", true, List.of(FILE, MODEL)),
-		IMPORT("I", "import", true, List.of(FILE, MODEL));
+		EXPORT("E", "export", true, List.of(MODEL)),
+		IMPORT("I", "import", true, List.of(MODEL));
 
 		static {
 			MODEL.acceptableVariants = List
 					.of(AppConstants.ModelType.CARS.name(), AppConstants.ModelType.STUDENTS.name(), AppConstants.ModelType.BARRELS.name());
-			LENGTH.conflictingSteps = List.of(DISPLAY);
-			DISPLAY.conflictingSteps = List.of(LENGTH);
-			EXPORT.conflictingSteps = List.of(IMPORT, CREATE);
-			IMPORT.conflictingSteps = List.of(EXPORT, CREATE);
-			CREATE.conflictingSteps = List.of(IMPORT, EXPORT);
 		}
 
 		public final String shortOpt;
@@ -141,12 +113,10 @@ public class CommandProcessor {
 		public final boolean hasArg;
 		public List<String> acceptableVariants;
 		public List<CommandStep> requiredSteps;
-		public List<CommandStep> conflictingSteps;
 
 		CommandStep(String shortOpt, String longOpt, boolean hasArg, List<CommandStep> requiredSteps) {
 			this(shortOpt, longOpt, hasArg);
 			this.requiredSteps = requiredSteps;
-			this.conflictingSteps = List.of();
 		}
 
 		CommandStep(String shortOpt, String longOpt, boolean hasArg) {
@@ -155,7 +125,6 @@ public class CommandProcessor {
 			this.hasArg = hasArg;
 			this.acceptableVariants = List.of();
 			this.requiredSteps = List.of();
-			this.conflictingSteps = List.of();
 		}
 	}
 }
