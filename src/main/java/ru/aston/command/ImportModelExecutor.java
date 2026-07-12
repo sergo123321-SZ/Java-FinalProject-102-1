65ffcfe5d777240cf679a557e78d1c1b71a5a883
Sergey Zastrovnyh <sergeizastrovnyh@yandex.com>
2026-07-12 18:10:05 +0600

    implement ImportModelExecutor logics
    add file handling
    add model type processing

--- src/main/java/ru/aston/command/ImportModelExecutor.java
+++ src/main/java/ru/aston/command/ImportModelExecutor.java
@@ -4,6 +4,11 @@ package ru.aston.command;
 import org.apache.commons.cli.CommandLine;
 import org.jetbrains.annotations.NotNull;
 
+import ru.aston.core.AppConstants.WriteMode;
+import ru.aston.jsonrw.readers.BarrelJsonReader;
+import ru.aston.jsonrw.readers.CarJsonReader;
+import ru.aston.jsonrw.readers.StudentJsonReader;
+
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
@@ -17,6 +22,25 @@ public class ImportModelExecutor extends BaseExecutor {
 
 	@Override
 	void doExec(@NotNull CommandLine options, @NotNull ExecutionData executionData) {
+		String filePath = Path.of(options.getOptionValue(CommandProcessor.CommandStep.IMPORT.shortOpt)).toAbsolutePath().toString();
+		boolean doAppend = executionData.writeMode == null || executionData.writeMode == WriteMode.APPEND;
+
+		switch (executionData.modelType) {
+			case BARRELS:
+				assert executionData.barrelCollection != null;
+				new BarrelJsonReader().readBarrelsFromFile(executionData.barrelCollection, filePath, doAppend);
+				break;
+			case CARS:
+				assert executionData.carCollection != null;
+				new CarJsonReader().readCarsFromFile(executionData.carCollection, filePath, doAppend);
+				break;
+			case STUDENTS:
+				assert executionData.studentCollection != null;
+				new StudentJsonReader().readStudentsFromFile(executionData.studentCollection, filePath, doAppend);
+				break;
+			default:
+				throw new IllegalArgumentException("'modelType' is unknown. MUST be assigned before execution!");
+		}
 
 	}
 
